115a9da745baf2f9135af028ed13aa5369ee0b58
maloo08 <lorena.kern@edu.fh-joanneum.at>
2019-01-16 16:54:50 +0100

    third commit

--- app/src/main/java/at/fh/swengb/kern/homeexercise2/NotesDao.kt
+++ app/src/main/java/at/fh/swengb/kern/homeexercise2/NotesDao.kt
@@ -1,8 +1,10 @@
 package at.fh.swengb.kern.homeexercise2
 
+import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
 
+@Dao
 interface NotesDao {
     @Insert
     fun insert(notes: Notes)
